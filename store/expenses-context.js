8ce5e64ce10c8fffc9ae962d76f3679a2657ab88
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-05-07 22:41:48 +0530

    Added delete and update button functionality

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,35 +1,59 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react';
 
 const DUMMY_EXPENSES = [
   {
-    id: "e1",
-    description: "A pair of Shoes",
+    id: 'e1',
+    description: 'A pair of shoes',
     amount: 59.99,
-    date: new Date("2021-12-19"),
+    date: new Date('2021-12-19'),
   },
   {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 29.99,
-    date: new Date("2021-01-22"),
+    id: 'e2',
+    description: 'A pair of trousers',
+    amount: 89.29,
+    date: new Date('2022-01-05'),
   },
   {
-    id: "e3",
-    description: "A pair of bananas",
+    id: 'e3',
+    description: 'Some bananas',
     amount: 5.99,
-    date: new Date("2021-12-01"),
+    date: new Date('2021-12-01'),
   },
   {
-    id: "e4",
-    description: "A pair of books",
+    id: 'e4',
+    description: 'A book',
     amount: 14.99,
-    date: new Date("2021-02-19"),
+    date: new Date('2022-02-19'),
   },
   {
-    id: "e5",
-    description: "Another of book",
-    amount: 18.99,
-    date: new Date("2021-02-18"),
+    id: 'e5',
+    description: 'Another book',
+    amount: 18.59,
+    date: new Date('2022-02-18'),
+  },
+  {
+    id: 'e6',
+    description: 'A pair of trousers',
+    amount: 89.29,
+    date: new Date('2022-01-05'),
+  },
+  {
+    id: 'e7',
+    description: 'Some bananas',
+    amount: 5.99,
+    date: new Date('2021-12-01'),
+  },
+  {
+    id: 'e8',
+    description: 'A book',
+    amount: 14.99,
+    date: new Date('2022-02-19'),
+  },
+  {
+    id: 'e9',
+    description: 'Another book',
+    amount: 18.59,
+    date: new Date('2022-02-18'),
   },
 ];
 
@@ -41,21 +65,20 @@ export const ExpensesContext = createContext({
 });
 
 function expensesReducer(state, action) {
-  switch (action.Type) {
-    case "ADD":
+  switch (action.type) {
+    case 'ADD':
       const id = new Date().toString() + Math.random().toString();
-      return [{ ...action, payload, id: id }, ...state];
-    case "UPDATE":
-      const updateableExpenseIndex = state.findIndex(
+      return [{ ...action.payload, id: id }, ...state];
+    case 'UPDATE':
+      const updatableExpenseIndex = state.findIndex(
         (expense) => expense.id === action.payload.id
       );
-      const updatableExpense = state[updateableExpenseIndex];
+      const updatableExpense = state[updatableExpenseIndex];
       const updatedItem = { ...updatableExpense, ...action.payload.data };
-      const updatedExpense = { ...state };
-      updatedExpense[updateableExpenseIndex] = updatedItem;
-      return updatedExpense;
-
-    case "DELETE":
+      const updatedExpenses = [...state];
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      return updatedExpenses;
+    case 'DELETE':
       return state.filter((expense) => expense.id !== action.payload);
     default:
       return state;
@@ -66,14 +89,15 @@ function ExpensesContextProvider({ children }) {
   const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
 
   function addExpense(expenseData) {
-    dispatch({ type: "ADD", payload: expenseData });
+    dispatch({ type: 'ADD', payload: expenseData });
   }
 
-  function deleteExpense(id,expenseData) {
-    dispatch({ type: "DELETE", payload: { id: id, data: expenseData } });
+  function deleteExpense(id) {
+    dispatch({ type: 'DELETE', payload: id });
   }
+
   function updateExpense(id, expenseData) {
-    dispatch({ type: "UPDATE", payload: id });
+    dispatch({ type: 'UPDATE', payload: { id: id, data: expenseData } });
   }
 
   const value = {
@@ -83,7 +107,11 @@ function ExpensesContextProvider({ children }) {
     updateExpense: updateExpense,
   };
 
-  return <ExpensesContext.Provider value={value}>{children}</ExpensesContext.Provider>;
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
 }
 
 export default ExpensesContextProvider;
